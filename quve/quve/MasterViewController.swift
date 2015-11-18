daba963e36aef5b78745f836d9dba88c6f872ede
James Bampoe <james_d_bampoe@hotmail.com>
2015-11-18 15:09:36 +0100

    Initial commit of master detail template project

---
+++ quve/quve/MasterViewController.swift
@@ -0,0 +1,108 @@
+//
+//  MasterViewController.swift
+//  quve
+//
+//  Created by James Bampoe on 15/11/15.
+//  Copyright © 2015 James Bampoe. All rights reserved.
+//
+
+import UIKit
+import MediaPlayer
+
+class MasterViewController: UITableViewController {
+
+    var detailViewController: DetailViewController? = nil
+    var objects = [AnyObject]()
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.navigationItem.leftBarButtonItem = self.editButtonItem()
+
+        let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
+        self.navigationItem.rightBarButtonItem = addButton
+        if let split = self.splitViewController {
+            let controllers = split.viewControllers
+            self.detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
+        }
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        self.clearsSelectionOnViewWillAppear = self.splitViewController!.collapsed
+        super.viewWillAppear(animated)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func insertNewObject(sender: AnyObject) {
+        if let nowPlayingItem = getNowPlayingItem(){
+            let playbackDuration = nowPlayingItem.playbackDuration
+            nowPlayingItem.mediaType
+            print("Current playback time:\(getCurrentPlaybackTime())\nAlbum artist:\(nowPlayingItem.albumArtist ?? "")\nTitle:\(nowPlayingItem.title ?? "")\n\(playbackDuration)")
+            objects.insert("\(nowPlayingItem.albumArtist ?? "") - \(nowPlayingItem.title ?? "")", atIndex: 0)
+            let indexPath = NSIndexPath(forRow: 0, inSection: 0)
+            self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+        }
+    }
+    
+    func getNowPlayingItem()->MPMediaItem?{
+        let systemMusicPlayer: MPMusicPlayerController = MPMusicPlayerController.systemMusicPlayer()
+        return systemMusicPlayer.nowPlayingItem
+    }
+    
+    func getCurrentPlaybackTime()->NSTimeInterval{
+        let systemMusicPlayer: MPMusicPlayerController = MPMusicPlayerController.systemMusicPlayer()
+        return systemMusicPlayer.currentPlaybackTime
+    }
+
+    // MARK: - Segues
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "showDetail" {
+            if let indexPath = self.tableView.indexPathForSelectedRow {
+                let object = objects[indexPath.row] as! String
+                let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
+                controller.detailItem = object
+                controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
+                controller.navigationItem.leftItemsSupplementBackButton = true
+            }
+        }
+    }
+
+    // MARK: - Table View
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return objects.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+
+        let object = objects[indexPath.row] as! String
+        cell.textLabel!.text = object
+        return cell
+    }
+
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            objects.removeAtIndex(indexPath.row)
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+        }
+    }
+}
+
